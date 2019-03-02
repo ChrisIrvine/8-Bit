eccd1db89446d6d2330fe8e325acb88896f6ec18
Ville Kuosmanen <vibe96@gmail.com>
2019-03-02 15:14:10 +0000

    Add skeleton

--- server/app.js
+++ server/app.js
@@ -1,19 +1,32 @@
 const express = require('express');
-const assert = require('assert');
-const MongoClient = require('mongodb').MongoClient;
+const app = express()
+const port = 5582
 
-const user = encodeURIComponent("8bitUser");
-const pw = encodeURIComponent("8bitPW");
-const url = 'mongodb://' + user + ":" + pw + '@vik.host.cs.st-andrews.ac.uk:27017/?authSource=8bit';
-const dbName = "8bit";
+app.use(express.static("public"));  //Assets folder
 
-console.log(url);
-const client = new MongoClient(url);
-client.connect(function(err) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-  
-    const db = client.db(dbName);
-  
-    client.close();
-  });
+app.get("/api/game/", getGame)
+app.post("/api/game/", postGame)
+
+function getGame(req, res) {
+    console.log(req.query.player1 + ", " + req.query.player2);
+    res.send("Hello World!");
+
+    //Get game id from ids
+
+    //Check if this game exists
+        //If yes, return it
+
+        //If not, create a simple one
+        //Commit to db
+        //return to client
+}
+
+function postGame(req, res) {
+    //TODO
+}
+
+app.listen(port, init);
+
+function init() {
+    console.log("listening!");
+}
